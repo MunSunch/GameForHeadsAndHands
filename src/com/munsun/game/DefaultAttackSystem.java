7710bd2b68dbe7ff1bec5b35691b94962096356b
MunirTheBest <msunchalyaev@gmail.com>
2023-09-25 23:32:38 +0300

    fix default attack system

--- src/com/munsun/game/DefaultAttackSystem.java
+++ src/com/munsun/game/DefaultAttackSystem.java
@@ -6,16 +6,21 @@ import java.util.Random;
 
 public class DefaultAttackSystem implements AttackSystem{
     private Random random;
+    private List<Integer> successValuesCube;
 
     public DefaultAttackSystem() {
         this.random = new Random();
+        this.successValuesCube = new ArrayList<>();
+            successValuesCube.add(5);
+            successValuesCube.add(6);
     }
 
     @Override
     public int attack(Entity entity, Entity anotherEntity) {
         int modifierAttack = entity.getValueAttack() - anotherEntity.getValueDefence() + 1;
         boolean success = roleCube(1);
-        success = roleCube(modifierAttack - 1);
+        if(modifierAttack > 0)
+            success = roleCube(modifierAttack - 1);
         if(success) {
             var damage = random.nextInt(entity.getMinDamage(), entity.getMaxDamage()+1);
             var newHealthAnotherEntity = anotherEntity.getHealth() - damage;
@@ -40,9 +45,6 @@ public class DefaultAttackSystem implements AttackSystem{
     }
 
     private boolean isSuccessValueCube(int currentValueCube) {
-        List<Integer> successValuesCube = new ArrayList<>();
-            successValuesCube.add(5);
-            successValuesCube.add(6);
         return successValuesCube.stream()
                 .anyMatch(x->x==currentValueCube);
     }
